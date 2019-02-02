71b4ceedc51300be253038ee39c50966b5179179
Riley Tziougras <rileyt2898@gmail.com>
2019-02-01 19:25:46 -0500

    arduino code to work with serial, and updated serial test

--- capstone_control.ino
+++ capstone_control.ino
@@ -1,46 +1,180 @@
 // Stepper - Version: Latest 
 #include <Stepper.h>
 
-char in_bytes[10];   // for incoming serial data
+char in_bytes[8];   // for incoming serial data
 const int start_seq = 36;
 const int end_seq = 47;
-int x_coord = 0;
-int y_coord = 0;
-int z_coord = 0;
+int v_ang = 0;
+int h_ang = 0;
+int v_steps = 0;
+int h_steps = 0;
+int count_h = 0;
+int count_v = 0;
 int ttf = 0xffff;
 int reply = 0; // for testing serial
 
-int ready = 0;
+const int pulPin = 10;
+const int dirPin = 11;
+const int enblPin = 12;
+const int pulPin_v = 7;
+const int dirPin_v = 8;
+const int enblPin_v = 9;
+
+int dly = 500;
+
+int ready_v = 0;
+int ready_h = 0;
+int finished = 1;
+int state = 0;
 
 void setup() {
-        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
-        pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  
+  pinMode(pulPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  pinMode(enblPin, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(2), h_lim_1, LOW); // pin 2
+  attachInterrupt(digitalPinToInterrupt(3), h_lim_2, LOW); // pin 3
+  pinMode(pulPin_v, OUTPUT);
+  pinMode(dirPin_v, OUTPUT);
+  pinMode(enblPin_v, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(18), v_lim_1, LOW); // pin 18
+  //attachInterrupt(digitalPinToInterrupt(19), v_lim_2, LOW); // pin 19
+  
+  digitalWrite(pulPin, LOW);
+  digitalWrite(enblPin, LOW);
+  digitalWrite(dirPin, LOW);
+  digitalWrite(pulPin_v, LOW);
+  digitalWrite(enblPin_v, LOW);
+  digitalWrite(dirPin_v, LOW);
+  
+  digitalWrite(enblPin, HIGH);
+  delay(100);
+  digitalWrite(enblPin, LOW);
+  digitalWrite(enblPin_v, HIGH);
+  delay(100);
+  digitalWrite(enblPin_v, LOW);
+  delay(2000);
 }
 
 void loop() {
-  if(!ready){
+  digitalWrite(dirPin, HIGH);
+  digitalWrite(dirPin_v, HIGH);
+  if(state == 0){
     digitalWrite(LED_BUILTIN, HIGH);
-    // do set-up sequence
-    // TODO: move motors to center position using limit switches
-    delay(1000); // 1 second for placeholder
-    ready = 1;
-  }
-    while(ready){
-      digitalWrite(LED_BUILTIN, LOW);
-      while (Serial.available()>=10){
-        Serial.readBytes(in_bytes,10);
-        if (start_seq == (int)in_bytes[0] && end_seq == (int)in_bytes[9]){
-          // a packet has been received so read data
-          ttf = ((int)in_bytes[1]<<8 & 0xff00) | ((int)in_bytes[2] & 0xff);
-          x_coord = ((int)in_bytes[3]<<8 & 0xff00) | ((int)in_bytes[4] & 0xff);
-          y_coord = ((int)in_bytes[5]<<8 & 0xff00) | ((int)in_bytes[6] & 0xff);
-          z_coord = ((int)in_bytes[7]<<8 & 0xff00) | ((int)in_bytes[8] & 0xff);
-        }
-      }
-      // convert coordinates to motor angles
-      // move motors
-      // if ttf
-        // fire
-        // ready = 0;
+    while(!ready_v){
+      // move limit switches until a limit switch is hit
+      digitalWrite(pulPin_v, HIGH);
+      digitalWrite(pulPin_v, LOW);
+      delayMicroseconds(dly);
     }
+    while(!ready_h){
+      // move limit switches until a limit switch is hit
+      digitalWrite(pulPin, HIGH);
+      digitalWrite(pulPin, LOW);
+      delayMicroseconds(dly);
+    }
+    state = 1;
+  }
+  
+  if(state == 1){
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(2000);
+    while (Serial.available()>=8){
+      Serial.readBytes(in_bytes,8);
+      if (start_seq == (int)in_bytes[0] && end_seq == (int)in_bytes[7]){
+        // a packet has been received so read data
+        //finished = 0;
+        state = 2;
+        ttf = ((int)in_bytes[1]<<8 & 0xff00) | ((int)in_bytes[2] & 0xff);
+        h_ang = ((int)in_bytes[3]<<8 & 0xff00) | ((int)in_bytes[4] & 0xff);
+        v_ang = ((int)in_bytes[5]<<8 & 0xff00) | ((int)in_bytes[6] & 0xff);
+      }
+    }
+    h_steps = 27*400/(360/abs(h_ang));
+    v_steps = 99.51*400/(360/abs(v_ang));
+    if(h_ang < 0){
+      digitalWrite(dirPin, HIGH); 
+    } else{
+      digitalWrite(dirPin, LOW); 
+    }
+    if(v_ang < 0){
+      digitalWrite(dirPin_v, HIGH); 
+    } else{
+      digitalWrite(dirPin_v, LOW); 
+    }
+  }
+  if(state == 2){
+    if(count_h<h_steps){ // if not in position yet
+      digitalWrite(pulPin, HIGH);
+      digitalWrite(pulPin, LOW);
+      count_h++;
+    }
+    if(count_v<v_steps){ // if not in position yet
+      digitalWrite(pulPin_v, HIGH);
+      digitalWrite(pulPin_v, LOW);
+      count_v++;
+    }
+    delayMicroseconds(dly);
+    if (!(count_h<h_steps) && !(count_v<v_steps)){
+      //fire
+      delay(3000);
+      state = 0; // later reset with timer
+      ready_v = 0; ready_h = 0;
+      count_h = 0; count_v = 0;
+    }
+  } // end of state 2
+} // end of loop()
+
+// Note: limit switches drive pins to GND
+void v_lim_1(){
+  // 1800 for horiz (60), 2488 for vert (22.5)
+  digitalWrite(LED_BUILTIN, LOW);
+  ready_v = 1;
+  dly = 800;
+  digitalWrite(dirPin_v, LOW);
+  for(int i = 0; i <= 4422; i++){
+    digitalWrite(pulPin_v, HIGH);
+    digitalWrite(pulPin_v, LOW);
+    delayMicroseconds(dly);
+  }
+}
+
+void v_lim_2(){
+  // 1800 for horiz (60), 2488 for vert (22.5)
+  digitalWrite(LED_BUILTIN, LOW);
+  ready_v = 1;
+  dly = 800;
+  digitalWrite(dirPin_v, LOW);
+  for(int i = 0; i <= 4422; i++){
+    digitalWrite(pulPin_v, HIGH);
+    digitalWrite(pulPin_v, LOW);
+    delayMicroseconds(dly);
+  }
+}
+
+void h_lim_1(){
+  //horizontal limit switch has been hit, too far cw
+  // 1800 for horiz (60), 2488 for vert (22.5)
+  ready_h = 1;
+  dly = 800;
+  digitalWrite(dirPin, HIGH);
+  for(int i = 0; i <= 1800; i++){ // move back to center
+    digitalWrite(pulPin, HIGH);
+    digitalWrite(pulPin, LOW);
+    delayMicroseconds(dly);
+  }
+}
+
+void h_lim_2(){
+  // 1800 for horiz (60), 2488 for vert (22.5)
+  ready_h = 1;
+  dly = 800;
+  digitalWrite(dirPin, LOW);
+  for(int i = 0; i <= 1800; i++){ // move back to center
+    digitalWrite(pulPin, HIGH);
+    digitalWrite(pulPin, LOW);
+    delayMicroseconds(dly);
+  }
 }
--- ser_comm_test/capstone_serial_test.py
+++ ser_comm_test/capstone_serial_test.py
@@ -3,26 +3,29 @@ import time
 import struct
 import random
 
-count = -32768
+count = 120
 successes = 0
 failures = 0
 ser = serial.Serial('COM3', 9600, timeout = 3)
 print(ser.name)
-time.sleep(2)
-while count < -32512:
-    y = count
-    ser.write(struct.pack('>bhhhhb',36,y,y,y,y,47))
-    s = ser.read(1)
-    if (ord(s) == 1):
-        successes += 1
-    else:
-        #print count
-        #print ord(s)
-        failures += 1
-    count+=1
+time.sleep(10)
+#while count < 10:
+y = count
+ser.write(struct.pack('>bhhhb',36,10,-20,-30,47))
+s = ser.read(1)
+#print ord(s)
+#if (ord(s) == 1):
+#    successes += 1
+#else:
+#print count
+#print ord(s)
+#    failures += 1
+count+=1
+time.sleep(15)
+ser.write(struct.pack('>bhhhb',36,10,20,30,47))
 
-print "number of successes"
-print successes
-print "number of failures"
-print failures
+#print "number of successes"
+#print successes
+#print "number of failures"
+#print failures
 ser.close()
